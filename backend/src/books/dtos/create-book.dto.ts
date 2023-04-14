6e756f2d49eb5ee122a77d39a9ae1548756cb9f0
Cassio Pieroni <cassiopieroni@gmail.com>
2023-04-14 16:32:26 -0300

    fix: remove min length from book dto

--- backend/src/books/dtos/create-book.dto.ts
+++ backend/src/books/dtos/create-book.dto.ts
@@ -1,16 +1,13 @@
-import { IsNotEmpty, MinLength } from 'class-validator';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateBookDto {
   @IsNotEmpty()
-  @MinLength(5)
   title: string;
 
   @IsNotEmpty()
-  @MinLength(3)
   bookPublisher: string;
 
   @IsNotEmpty()
-  @MinLength(4)
   author: string;
 
   @IsNotEmpty()
